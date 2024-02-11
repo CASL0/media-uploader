8c5d5c021835a256919a8c9c6b79912f9a7ad73b
CASL0 <gin.0046.822@gmail.com>
2024-02-11 19:50:56 +0900

    feat: 通知チャネルのIDと名前

---
+++ app/src/main/java/io/github/casl0/mediauploader/utils/Constants.kt
@@ -0,0 +1,9 @@
+package io.github.casl0.mediauploader.utils
+
+import io.github.casl0.mediauploader.R
+
+/** メディアファイル監視用サービスの通知チャネルのID */
+const val OBSERVER_SERVICE_CHANNEL_ID = "media_observer"
+
+/** メディアファイル監視用サービスの通知チャネル名 */
+val OBSERVER_SERVICE_CHANNEL_NAME = R.string.service_channel_name
