9a33558dfc065df0011cbf09c4806d272b8af943
CASL0 <gin.0046.822@gmail.com>
2024-02-11 20:24:40 +0900

    feat: compose navigation用のルート

---
+++ app/src/main/java/io/github/casl0/mediauploader/ui/MediaUploaderRoute.kt
@@ -0,0 +1,15 @@
+package io.github.casl0.mediauploader.ui
+
+import androidx.annotation.StringRes
+import io.github.casl0.mediauploader.R
+
+/**
+ * ナビゲーション用のルート
+ *
+ * @property title AppBarに表示する文言
+ */
+internal enum class MediaUploaderRoute(
+    @StringRes val title: Int,
+) {
+    Home(R.string.home_title),
+}
