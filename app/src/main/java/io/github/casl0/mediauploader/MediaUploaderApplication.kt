9de3eb9d1884af580fd272f12f1d142f23cabad5
CASL0 <gin.0046.822@gmail.com>
2024-02-25 14:43:38 +0900

    fix: hiltの設定

---
+++ app/src/main/java/io/github/casl0/mediauploader/MediaUploaderApplication.kt
@@ -0,0 +1,8 @@
+package io.github.casl0.mediauploader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MediaUploaderApplication : Application() {
+}
