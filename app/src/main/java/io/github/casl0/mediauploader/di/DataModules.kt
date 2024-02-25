59848810e32f350418e744c10587122796702c78
CASL0 <gin.0046.822@gmail.com>
2024-02-25 14:28:03 +0900

    feat: データソースとリポジトリのDIモジュール

--- app/src/main/java/io/github/casl0/mediauploader/di/DataModules.kt
+++ app/src/main/java/io/github/casl0/mediauploader/di/DataModules.kt
@@ -7,7 +7,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.github.casl0.mediauploader.data.MediaUploaderDataSource
+import io.github.casl0.mediauploader.data.local.MediaUploaderLocalDataSource
 import io.github.casl0.mediauploader.database.MediaUploaderDatabase
+import io.github.casl0.mediauploader.repository.DefaultUpdateHistoryRepository
+import io.github.casl0.mediauploader.repository.UpdateHistoryRepository
 import javax.inject.Singleton
 
 /** データベース用のモジュール */
@@ -23,5 +27,31 @@ object DatabaseModule {
             MediaUploaderDatabase::class.java,
             "media_uploader.db"
         ).build()
+    }
+}
+
+/** データソースのDIモジュール */
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+    /** [MediaUploaderDataSource]のDI */
+    @Singleton
+    @Provides
+    fun provideUpdateHistoryDataSource(database: MediaUploaderDatabase): MediaUploaderDataSource {
+        return MediaUploaderLocalDataSource(database)
+    }
+}
+
+/** リポジトリのDIモジュール */
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    /** [UpdateHistoryRepository]のDI */
+    @Singleton
+    @Provides
+    fun provideUpdateHistoryRepository(
+        dataSource: MediaUploaderDataSource,
+    ): UpdateHistoryRepository {
+        return DefaultUpdateHistoryRepository(dataSource)
     }
 }
