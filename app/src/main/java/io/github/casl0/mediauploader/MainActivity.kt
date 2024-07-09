f0b0ddb793c431637f4b0e7b956381b4742a2d5f
CASL0 <gin.0046.822@gmail.com>
2024-07-09 22:39:22 +0900

    fix: selected photoに対応

--- app/src/main/java/io/github/casl0/mediauploader/MainActivity.kt
+++ app/src/main/java/io/github/casl0/mediauploader/MainActivity.kt
@@ -152,7 +152,14 @@ class MainActivity : ComponentActivity() {
 
     /** メディアパーミッションを要求します。 */
     private fun askMediaPermissions() {
-        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            listOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO,
+                Manifest.permission.READ_MEDIA_AUDIO,
+                Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED,
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             listOf(
                 Manifest.permission.READ_MEDIA_IMAGES,
                 Manifest.permission.READ_MEDIA_VIDEO,
